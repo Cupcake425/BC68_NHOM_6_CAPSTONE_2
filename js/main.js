c3b23bdf34595211fb18aaebcf0427597c6ad315
cupcake452 <tnv.conmeo3c@gmail.com>
2024-06-27 13:09:47 +0700

    thêm responsive navbar

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ window.addEventListener("scroll", function () {
   let header = this.document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function showSideBar() {
+  let sideBar = document.querySelector(".sidebar");
+  sideBar.style.display = "flex";
+}
+
+function hideSideBar() {
+  let sideBar = document.querySelector(".sidebar");
+  sideBar.style.display = "none";
+}
