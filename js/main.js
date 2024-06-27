d3fba507e2827bdc044f4b06e31d3cc1f6f3ae7a
cupcake452 <tnv.conmeo3c@gmail.com>
2024-06-27 18:15:19 +0700

    finish header and footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // chỉnh sticky cho header khi scroll
-window.addEventListener("scroll", function () {
-  let header = this.document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
-});
+// window.addEventListener("scroll", function () {
+//   let header = this.document.querySelector("header");
+//   header.classList.toggle("sticky", window.scrollY > 0);
+// });
 
 function showSideBar() {
   let sideBar = document.querySelector(".sidebar");
