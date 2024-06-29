5aa3282def054d6fcbba8f7791209ccc5763d435
cupcake452 <tnv.conmeo3c@gmail.com>
2024-06-29 20:41:23 +0700

    test call api xuong

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
-// chỉnh sticky cho header khi scroll
-// window.addEventListener("scroll", function () {
-//   let header = this.document.querySelector("header");
-//   header.classList.toggle("sticky", window.scrollY > 0);
-// });
-
+// Show Side bar
 function showSideBar() {
   let sideBar = document.querySelector(".sidebar");
   sideBar.style.display = "flex";
 }
 
+// giấu side bar
 function hideSideBar() {
   let sideBar = document.querySelector(".sidebar");
   sideBar.style.display = "none";
+}
+
+// Lấy danh sách giày
+function layDanhSachGiay() {
+  let promise = axios({
+    method: "GET",
+    url: "https://shop.cyberlearn.vn/api/Product",
+  });
+  promise
+    .then((res) => {
+      renderGiay(res.data.content);
+    })
+    .catch((err) => {
+      console.log("Có lỗi xảy ra");
+    });
+}
+layDanhSachGiay();
+
+// Chức năng hiển thị thông tin giày lên web
+function renderGiay(arr) {
+  let content = "";
+  arr.forEach((item) => {
+    let { name, price, shortDescription, image } = item;
+
+    content += `<div class="col-12 col-md-6 col-lg-4">
+            <div class="product_item">
+            <img src="${image}" alt="" />
+            <div class="product_buynow">
+              <a href="">BUY NOW</a>
+            </div>
+             </div>
+            <div class="product_info">
+              <p>${name}</p>
+              <p>${shortDescription}</p>
+              <p>${price}$</p>
+            </div>
+            
+          </div>`;
+  });
+  document.getElementById("product").innerHTML = content;
 }
